50bbd00beca1ba77eec59ea59a005fd0e09c3801
RodrigocAragao <rodrigocca08@gmail.com>
2024-10-28 16:03:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,117 @@ function moveSlide(step) {
     const translateXValue = -currentIndex * 100; // Cada slide ocupa 100% da largura
     carousel.style.transform = `translateX(${translateXValue}%)`;
 }
+
+
+window.onload = function() {
+
+    const ctxEficacia = document.getElementById('graficoEficacia').getContext('2d');
+    new Chart(ctxEficacia, {
+        type: 'bar',
+        data: {
+            labels: ['Quantidade de cabelos perdidos', 'Aparência geral dos cabelos'],
+            datasets: [
+                {
+                    label: 'Melhora',
+                    data: [66, 69],
+                    backgroundColor: 'rgba(75, 192, 192, 0.7)',
+                },
+                {
+                    label: 'Igual',
+                    data: [34, 31],
+                    backgroundColor: 'rgba(255, 168, 168, 0.7)',
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 100,
+                    ticks: {
+                        callback: (value) => value + '%',
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Eficácia Clínica',
+                    font: {
+                      size: 16,
+                    }
+                }
+            }
+        }
+    })
+            
+                  // Gráfico de Apreciabilidade Cosmética
+    const ctxApreciabilidade = document.getElementById('graficoApreciabilidade').getContext('2d');
+    new Chart(ctxApreciabilidade, {
+        type: 'bar',
+        data: {
+            labels: [
+                'Aumento na quantidade de cabelos',
+                'Fios mais grossos',
+                'Aumento no volume',
+                'Fortalecimento dos fios',
+                'Estímulo ao crescimento',
+                'Bom resultado',
+                'Compraria o produto'
+            ],
+            datasets: [
+                {
+                    label: 'Sim',
+                    data: [84, 78, 81, 81, 84, 81, 81],
+                    backgroundColor: 'rgba(75, 192, 192, 0.7)',
+                },
+                {
+                    label: 'Não',
+                    data: [16, 22, 19, 19, 16, 19, 19],
+                    backgroundColor: 'rgba(255, 168, 168, 0.7)',
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 100,
+                    ticks: {
+                        callback: (value) => value + '%',
+                    },
+                },
+                x: {
+                    ticks: {
+                        maxRotation: 90,
+                        minRotation: 90,
+                        callback: function(value) {
+                            const words = this.getLabelForValue(value).split(' ');
+                            return words.reduce((acc, word, index) => {
+                                return acc + (index % 2 === 0 ? '\n' : ' ') + word;
+                            })
+                        }
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Avaliação de Aceitabilidade e Resultados Percebidos',
+                    font: {
+                      size: 16,
+                    }
+                }
+            }
+        }
+    })
+}
+            
